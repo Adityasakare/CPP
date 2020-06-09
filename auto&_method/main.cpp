e203bdbecb95aea0aee31837da857e6010ea975b
Aditya Sakare <aditya.sakare16@gmail.com>
2020-06-09 23:41:29 +0530

    Auto_method

---
+++ auto&_method/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A[2][3];
+    int n;
+
+
+    for(auto& x:A)
+    {
+        for (auto& y:x)
+        {
+            cin>>y;
+        }
+        cout<<endl;
+    }
+
+
+    for(auto& x:A)
+    {
+        for (auto& y:x)
+        {
+            cout<<y<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
