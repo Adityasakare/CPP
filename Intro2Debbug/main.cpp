bca49357933f545d6c30cf29c554f35edebae8cb
Aditya Sakare <aditya.sakare16@gmail.com>
2020-06-23 23:57:43 +0530

    debug

---
+++ Intro2Debbug/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+
+int main()
+{
+    int a, b, c;
+    std::cout << "Enter a number " ;
+    std::cin >> a;
+    std::cout <<"Enter another Number";
+    std::cin >> b;
+    c = a+b;
+    std::cout<<" Sum is "<<c;
+    return 0;
+}
